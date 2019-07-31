89807d3c59bdbe542d14250b2673acb435171163
Temirlan Abilkhaiyr <abilkhairov17@gmail.com>
2019-07-31 19:16:13 +0600

    UPDATE: Added API endpoints

--- lib/data/network/repository/time_api.dart
+++ lib/data/network/repository/time_api.dart
@@ -12,10 +12,84 @@ class TimeAPI {
 
   NetworkCall _networkCall = NetworkCall();
 
-  static final ALL_SERVICES = '/getMain';
-  static final ACTUAL_SERVICES = '/getActual';
-  static final SUBSERVICES = '/getItem';
-  static final MASTERS = '/getListOfMasterOnline';
+  // Authorization
+  static const AUTHORIZE = '/oauth/token';
+  static const CHECK_TOKEN = '/oauth/check_token';
+
+  // Common
+  static const INFRASTRUCTURE = '/infrastructure/all';
+  static const CATEGORIES = '/category/all';
+  static const ALL_SERVICES = '/category/service/all';
+  static const ACTUAL_SERVICES = '/actualService/all';
+
+  // Specialist
+  static const SPECIALIST_REGISTER_OR_REFRESH_PASSWORD = '/specialist/profile/registerOrRefreshPassword';
+  static const SPECIALIST_PROFILE_GET = '/specialist/profile/get';
+  static const SPECIALIST_PROFILE_EDIT = '/specialist/profile/edit';
+  static const SPECIALIST_SERVICES_GET_ALL = '/specialist/specialistService/all';
+  static const SPECIALIST_SERVICE_GET = '/specialist/specialistService/get';
+  static const SPECIALIST_SERVICE_ADD = '/specialist/specialistService/add';
+  static const SPECIALIST_SERVICE_EDIT = '/specialist/specialistService/edit';
+  static const SPECIALIST_SERVICE_DELETE = '/specialist/specialistService/delete';
+  static const SPECIALIST_SET_ONLINE = '/specialist/specialistService/setOnline';
+  static const SPECIALIST_SET_OFFLINE = '/specialist/specialistService/setOffline';
+  static const SPECIALIST_NOTIFICATIONS_GET = '/specialist/specialistService/notification/all';
+  static const SPECIALIST_NOTIFICATION_DELETE = '/specialist/specialistService/notification/delete';
+  static const SPECIALIST_ORDER_HISTORY_GET_ALL = '/specialist/specialistService/order/history/all';
+  static const SPECIALIST_ORDER_HISTORY_DELETE = '/specialist/specialistService/order/history/delete';
+  static const SPECIALIST_ORDER_CURRENT_GET_ALL = '/specialist/specialistService/order/current/all';
+  static const SPECIALIST_ORDER_CURRENT_DELETE = '/specialist/specialistService/order/current/delete';
+  static const SPECIALIST_ORDER_GET = '/specialist/specialistService/order/get';
+  static const SPECIALIST_ORDER_APPROVE = '/specialist/specialistService/order/approve';
+  static const SPECIALIST_ORDER_REJECT = '/specialist/specialistService/order/reject';
+  static const SPECIALIST_ORDER_RATE = '/specialist/specialistService/order/rate';
+  static const SPECIALIST_ORDER_REPORT = '/specialist/specialistService/order/report';
+  static const SPECIALIST_REVIEW_GET_ALL = '/specialist/specialistService/review/all';
+  static const SPECIALIST_STORY_GET_ALL = '/specialist/story/all';
+  static const SPECIALIST_STORY_ADD = '/specialist/story/add';
+  static const SPECIALIST_STORY_DELETE = '/specialist/story/delete';
+  static const SPECIALIST_CHAT_GET_ALL = '/specialist/chat/all';
+  static const SPECIALIST_CHAT_GET = '/specialist/chat/get';
+  static const SPECIALIST_CHAT_DELETE = '/specialist/chat/delete';
+  static const SPECIALIST_CHAT_MESSAGE_SEND = '/specialist/chat/sendMessage';
+  static const SPECIALIST_CHAT_MESSAGE_DELETE = '/specialist/chat/deleteMessage';
+
+  // Customer
+  static const CUSTOMER_REGISTER_OR_REFRESH_PASSWORD = '/customer/profile/registerOrRefreshPassword';
+  static const CUSTOMER_PROFILE_GET = '/customer/profile/get';
+  static const CUSTOMER_PROFILE_EDIT = '/customer/profile/edit';
+  static const CUSTOMER_SPECIALIST_SERVICE_GET_ALL = '/customer/specialistService/all';
+  static const CUSTOMER_SPECIALIST_SERVICE_GET = '/customer/specialistService/get';
+  static const CUSTOMER_SPECIALIST_SERVICE_SEARCH = '/customer/specialistService/search';
+  static const CUSTOMER_SPECIALIST_SERVICE_REPORT = '/customer/specialistService/report';
+  static const CUSTOMER_SPECIALIST_SERVICE_REVIEW_GET_ALL = '/customer/specialistService/review/all';
+  static const CUSTOMER_SPECIALIST_SERVICE_REVIEW_ADD = '/customer/specialistService/review/add';
+  static const CUSTOMER_NOTIFICATION_GET_ALL = '/customer/notification/all';
+  static const CUSTOMER_NOTIFCATION_DELETE = '/customer/notification/delete';
+  static const CUSTOMER_ORDER_CREATE = '/customer/order/create';
+  static const CUSTOMER_ORDER_GET = '/customer/order/get';
+  static const CUSTOMER_ORDER_CANCEL = ' /customer/order/cancel';
+  static const CUSTOMER_ORDER_HISTORY_GET_ALL = '/customer/order/history/all ';
+  static const CUSTOMER_ORDER_HISTORY_DELETE = '/customer/order/history/delete';
+  static const CUSTOMER_ORDER_RATE = '/customer/order/rate';
+  static const CUSTOMER_FAVOURITE_SPECIALIST_GET_ALL = '/customer/favoriteSpecialist/all';
+  static const CUSTOMER_FAVOURITE_SPECIALIST_ADD = ' /customer/favoriteSpecialist/add';
+  static const CUSTOMER_FAVOURITE_SPECIALIST_DELETE = '/customer/favoriteSpecialist/remove';
+  static const CUSTOMER_SPECIALIST_STORY_GET_ALL = '/customer/specialist/story/all';
+  static const CUSTOMER_CHAT_GET_ALL = '/customer/chat/all';
+  static const CUSTOMER_CHAT_GET = '/customer/chat/get';
+  static const CUSTOMER_CHAT_DELETE = '/customer/chat/delete';
+  static const CUSTOMER_CHAT_MESSAGE_SEND = '/customer/chat/sendMessage';
+  static const CUSTOMER_CHAT_MESSAGE_DELETE = '/customer/chat/deleteMessage';
+
+
+
+
+
+
+
+
+
 
   Future<List<ServicesResponse>> getAllServices() async {
 
@@ -49,7 +123,7 @@ class TimeAPI {
   }
 
   Future<List<ServicesResponse>> getSubServicesByType(String serviceType) async {
-    String requestUrl = SUBSERVICES + '/$serviceType';
+    String requestUrl = '/$serviceType';
 
     dynamic response = await _networkCall.doRequest(path: requestUrl, method: 'GET', context: null);
 
@@ -67,7 +141,7 @@ class TimeAPI {
 
   Future<List<MastersResponse>> getMasters(String serviceType) async {
 
-    dynamic response = await _networkCall.doRequest(path: MASTERS, method: 'GET', context: null);
+    dynamic response = await _networkCall.doRequest(path: '', method: 'GET', context: null);
 
     List<MastersResponse> servicesList = List();
 
